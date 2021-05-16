3cc8ab9dcbb6a40e11d8515495a9783fd1ad6a18
fumika88 <fuum14532@gmail.com>
2021-05-16 03:12:57 +0000

    レイアウト修正

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,5 @@
 class PhotosController < ApplicationController
+  before_action :authenticate_user!,except: [:top]
 
   def hashtag
     @user = current_user
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
-
+  before_action :authenticate_user!
   def show
     @user = User.find(params[:id])
     #pageメソッドを利用して、すべての投稿画像が表示されないように設定
     @photos = @user.photos.page(params[:page]).reverse_order
+    #@photos = @user.photos
   end
 
   def index
