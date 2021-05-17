a159e536a13876a3ebe452289bc90ed6e08f67a4
fumika88 <fuum14532@gmail.com>
2021-05-17 05:12:43 +0000

    コメント機能

---
+++ test/controllers/photo_comments_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class PhotoCommentsControllerTest < ActionDispatch::IntegrationTest
+  test "should get create" do
+    get photo_comments_create_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get photo_comments_destroy_url
+    assert_response :success
+  end
+
+end
